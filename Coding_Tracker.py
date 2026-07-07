cf5ccc6511ae32a58abeb3f3cea3c4e6e016bcd5
Posi10 <ayomiposiawojide@gmail.com>
2026-07-07 16:34:07 -0400

    Sessions shown by number

--- Coding_Tracker.py
+++ Coding_Tracker.py
@@ -1,50 +1,58 @@
 import time
 Hours = []
+
 def load_data():
-    try:
-     with open("Coding Tracker V2.txt", "r") as file:
-        for line in file:
-            Hours.append(int(line.strip()))
-    except FileNotFoundError:
-       time.sleep(3)
-       print("Welcome to Coding Tracker V2!")
-       time.sleep(3)
-       print("To start please pick option 1 so we can load your sessions!")
+   try:
+      with open("Coding Tracker V2.txt", "r") as file:
+         for line in file:
+            if line.strip():
+               Hours.append(int(line.strip()))
+   except FileNotFoundError:
+      time.sleep(3)
+      print("Welcome to Coding Tracker V2!")
+      time.sleep(3)
+      print("To start please pick option 1 so we can load your sessions!")
+
 
 def save_data():
    with open("Coding Tracker V2.txt", "a") as file:
       for Hour in Hours:
          file.write(str(Hour) + "\n")
 
+
 def show_menu():
-    print("\n--------------")
-    print("--Coding Traker V2")
-    print("--------------------")
-    print("\n1. Log Session")
-    print("2. Show Session")
-    print("3. Show statistics")
-    print("4. Quit")
+   print("\n--------------")
+   print("--Coding Tracker V2")
+   print("--------------------")
+   print("\n1. Log Session")
+   print("2. Show Session")
+   print("3. Show statistics")
+   print("4. Quit")
+
 
 def get_choice():
-    return input("Pick an option: ")
-    
+   return input("Pick an option: ")
+
+
 def log_session():
-    Hour = int(input("Hours coded: "))
-    Hours.append(Hour)
-    print("Hours logged")
+   Hour = int(input("Hours coded: "))
+   Hours.append(Hour)
+   print("Hours logged")
+
 
 def show_sessions():
    if len(Hours) == 0:
       print("No hours logged")
    else:
-      for Hour in Hours:
-         print(Hour)
+      for index, Hour in enumerate(Hours, start=1):
+         print(f"Session {index}: {Hour} hours.")
+
 
 def show_statistics():
    if len(Hours) == 0:
-         print("No hours logged")
+      print("No hours logged")
    else:
-      Average = sum(Hours)/len(Hours)
+      Average = sum(Hours) / len(Hours)
       Total = sum(Hours)
       Highest = max(Hours)
       Least = min(Hours)
@@ -53,6 +61,7 @@ def show_statistics():
       print(f"The most you coded in a session was {Highest} hours.")
       print(f"the least you coded in a session was {Least} hours.")
 
+
 def main():
    while True:
       show_menu()
@@ -60,7 +69,7 @@ def main():
       if Choice == "1":
          log_session()
       elif Choice == "2":
-       show_sessions()
+         show_sessions()
       elif Choice == "3":
          show_statistics()
       elif Choice == "4":
@@ -69,4 +78,4 @@ def main():
          break
       else:
          print("Invalid option")
-main()
+   main()
